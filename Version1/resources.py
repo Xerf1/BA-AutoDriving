03c3c9b3dbbdba44e64b760b732e6a430c1f1f3e
Paul Flachbart <paul.flachbart@hpe.com>
2019-07-01 08:45:38 +0200

    ...beim letzten commit waren überall noch merge statements in den python files ...

--- Version1/resources.py
+++ Version1/resources.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pyglet 
 pyglet.resource.path = ['../Resources']
 pyglet.resource.reindex()
@@ -12,19 +11,4 @@ def center_image(image):
 	image.anchor_x = image.width/2
 	image.anchor_y = image.height/2
 
-=======
-import pyglet 
-pyglet.resource.path = ['../Resources']
-pyglet.resource.reindex()
-
-car_image = pyglet.resource.image("car.png")
-car_image.width = 50
-car_image.height = 25
-
-def center_image(image):
-	"""Sets an image's anchor point to its center"""
-	image.anchor_x = image.width/2
-	image.anchor_y = image.height/2
-
->>>>>>> 0e13d0c6f304902f2074cab7ccbea29ca4280fb4
 center_image(car_image)
